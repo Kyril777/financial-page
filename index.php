f16b85bba3c28150a431bffa76a4425795a5fabd
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-03-12 23:32:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta charset="utf-8" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
 		<script src="http://cdn.zingchart.com/zingchart.min.js"></script>
 
 	</head>
